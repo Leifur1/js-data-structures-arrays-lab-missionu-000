41009c1c62527b4ccff80b24ff0f633b5d341f2e
Leifur Sigurdsson <leifurerik@gmail.com>
2019-03-05 20:11:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ function appendDriver(name) {
 function prependDriver(name) {
   return [name, ...drivers]
 }
+
+function removeLastDriver() {
+  return drivers.slice(1)
+}
+
+function removeLastDriver() {
+  return drivers.slice(0, driver.length - 1)
+}
+
+
+
